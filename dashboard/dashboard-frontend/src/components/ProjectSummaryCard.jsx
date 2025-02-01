2514341a57deb11956220b169d4c2144f2b9c062
cdpatra <cdpatra03@gmail.com>
2025-02-01 15:14:26 +0530

    implement the form component

--- dashboard/dashboard-frontend/src/components/ProjectSummaryCard.jsx
+++ dashboard/dashboard-frontend/src/components/ProjectSummaryCard.jsx
@@ -5,7 +5,7 @@ function ProjectSummaryCard({ items }) {
             {
                 items.map((item, i) => {
                     return <div key={i} className="tile-container flex justify-between items-center p-2">
-                        <div className="wrapper flex items-center gap-2">
+                        <div className="flex items-center gap-2">
                             <div className="icon w-8 h-8 rounded-full flex items-center justify-center" style={{ color: `${item.color}`, backgroundColor: `${item.color}30` }}>
                                 {item.icon}
                             </div>
--- dashboard/dashboard-frontend/src/forms/Form.jsx
+++ dashboard/dashboard-frontend/src/forms/Form.jsx
@@ -3,9 +3,25 @@ import React from 'react'
 function Form() {
   return (
     <section>
-      <div className="wrapper p-4">
-        <div className="form-container p-4 bg-white rounded-3xl">
-            Form
+      <div className="wrapper p-4 overflow-y-auto">
+        <div className="form-container p-4 rounded-3xl bg-white flex flex-col justify-center items-center">
+          <div className="form-title text-xl font-bold text-center">
+            Form title
+          </div>
+          <div className="form-wrapper border border-neutral-400 rounded-2xl p-2 w-full max-w-2xl">
+            <form className='bg-neutral-100 p-6 rounded-2xl' action="">
+              <div className='field-container'>
+                <label className='text-sm' htmlFor="name">Enter Your name</label>
+                <br />
+                <input className='bg-white w-full border border-slate-400 outline-none rounded-md p-1' id='name' type="text" />
+              </div>
+              <div className="field-container">
+                <label className='text-sm' htmlFor="message">Enter Your name</label>
+                <br />
+                <textarea className='bg-white w-full border border-slate-400 outline-none rounded-md p-1' name="" id="message"></textarea>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </section>
